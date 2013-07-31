65af71cb96b54f977a670b91bfa4ef9a87250ae5
Ada Rose Edwards <adaroseedwards@gmail.com>
2013-07-31 17:18:26 +0100

    Updated 0 to make it restful

--- app.js
+++ app.js
@@ -59,7 +59,22 @@ app.get('*', function (req,res) {
   if (fs.existsSync(publicURL)) {
     res.sendfile(publicURL);
   } else {
-    res.render('404');
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+      res.render('404', { url: req.url });
+      return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+      res.send({ error: 'Not found' });
+      return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
   }
 });
 
