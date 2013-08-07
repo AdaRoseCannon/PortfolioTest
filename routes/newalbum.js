3842853d07570b140115435091c47148544e9a2c
Ada Rose Edwards <adaroseedwards@gmail.com>
2013-08-07 22:58:12 +0100

    Can now make albums

--- routes/newalbum.js
+++ routes/newalbum.js
@@ -1,21 +1,27 @@
+var fs = require('fs');
+
 module.exports = function(req, res, callback){
 	var album;
 	if (req.query.album) album = req.query.album;
 		else  album = req.body.album;
-	var albumPath = fs.realpathSync(__dirname + "/../data/album/");
-	targetalbum = albumPath + "/" + albumName + ".js";
+	if (req.query.data) currentData = req.query.data
+		else currentData = req.body.data;
+
+	var newName = album.replace(/([^\w\d\.\-_~,;:\[\]\(\]]|[\.]{2,})/gi, '').toLowerCase();
+
+	var albumPath = fs.realpathSync(__dirname + "/../data") + "/album/";
+	targetalbum = albumPath + "/" + newName + ".js";
 
 	if (!fs.existsSync(albumPath)) {
-		fs.mkdir(albumPath, function () {
-			//Create album
-			fs.writeFile(targetalbum, JSON.stringify(currentData, null, "\t"), function(err) {
-				if(err) {
-					console.log("Could not save JSON: " + dataFile);
-					console.log(err);
-				} else {
-					callback({success: true});
-				}
-			});
-		});
+		fs.mkdirSync(albumPath)
 	}
+	//Create album
+	fs.writeFile(targetalbum, JSON.stringify(currentData, null, "\t"), function(err) {
+		if(err) {
+			console.log("Could not save JSON: " + dataFile);
+			console.log(err);
+		} else {
+			callback({success: newName});
+		}
+	});
 };
