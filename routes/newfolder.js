99c96179ebc96c2c4df0e87dd46b0238b04e0dfb
Ada Rose Edwards <adaroseedwards@gmail.com>
2013-08-08 08:26:50 +0100

    Sanitize folder creation.

--- routes/newfolder.js
+++ routes/newfolder.js
@@ -5,6 +5,9 @@ module.exports = function(req, res, callback){
 	var folder;
 	if (req.query.folder) folder = req.query.folder;
 		else  folder = req.body.folder;
+
+	folder = folder.replace(/([^\w\d\.\-_~,;:\[\]\(\]]|[\.]{2,})/gi, '').toLowerCase();
+
 	var rawPath = fs.realpathSync(__dirname + "/../data/raw/");
 	targetFolder = rawPath + "/" + folder + "/";
 	if (!fs.existsSync(targetFolder)){
